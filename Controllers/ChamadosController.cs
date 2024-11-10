9bd26e79e43021f231c8d44fc593596905280b29
evaldo <evaaldormj@gmail.com>
2024-11-09 22:08:57 -0300

    Feat: Add delete and put endpoints

--- Controllers/ChamadosController.cs
+++ Controllers/ChamadosController.cs
@@ -33,10 +33,87 @@ namespace IntegracaoChamados.Controllers
             }
             catch (Exception ex)
             {
-                _logger.LogInformation($"ERRO AO CONSULTAR TODOS OS CHAMADOS: {ex.Message}");
+                _logger.LogError($"ERRO AO CONSULTAR TODOS OS CHAMADOS: {ex.Message}");
                 return StatusCode(500, $"ERRO AO CONSULTAR TODOS OS CHAMADOS: {ex.Message}");
             }
         }
 
+        [HttpPost]
+        public async Task<ActionResult> CreateChamado([FromBody] Chamado chamado)
+        {
+            try
+            {
+                var sql = "INSERT INTO CHAMADOS (TITULO, AREA, SITUACAO, DATAABERTURA) VALUES (@TITULO, @AREA, @SITUACAO, @DATAABERTURA)";
+
+                await _connection.ExecuteAsync(sql, new
+                {
+                    TITULO = chamado.TITULO,
+                    AREA = chamado.AREA,
+                    SITUACAO = chamado.SITUACAO,
+                    DATAABERTURA = chamado.DATAABERTURA
+                });
+
+                _logger.LogInformation($"CHAMADO ABERTO COM SUCESSO: {chamado.TITULO}");
+
+                return Ok("CHAMADO ABERTO COM SUCESSO");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERRO AO ABRIR O CHAMADO: {ex.Message}");
+                return StatusCode(500, $"ERRO AO ABRIR O CHAMADO: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateChamado(int id, Chamado chamado)
+        {
+            {
+                try
+                {
+                    var sql = "UPDATE CHAMADOS SET TITULO = @TITULO, AREA = @AREA, SITUACAO = @SITUACAO, DATAABERTURA = @DATAABERTURA WHERE ID = @ID";
+
+                    await _connection.ExecuteAsync(sql, new
+                    {
+                        ID = id,
+                        TITULO = chamado.TITULO,
+                        AREA = chamado.AREA,
+                        SITUACAO = chamado.SITUACAO,
+                        DATAABERTURA = chamado.DATAABERTURA
+                    }
+                    );
+
+                    _logger.LogInformation("CHAMADO ATUALIZADO COM SUCESSO");
+                    return Ok("CHAMADO ATUALIZADO COM SUCESSO");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"ERRO AO ATUALIZAR O CHAMADO: {ex.Message}");
+                    return StatusCode(500, $"ERRO AO ATUALIZAR O CHAMADO: {ex.Message}");
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteChamado(int id)
+        {
+            try
+            {
+                var sql = "DELETE FROM CHAMADOS WHERE ID = @ID";
+
+                await _connection.ExecuteAsync(sql, new
+                    {
+                        ID = id,
+                    }
+                );
+
+                _logger.LogInformation("CHAMADO APAGADO COM SUCESSO");
+                return Ok("CHAMADO APAGADO COM SUCESSO");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERRO AO APAGAR O CHAMADO: {ex.Message}");
+                return StatusCode(500, $"ERRO AO APAGAR O CHAMADO: {ex.Message}");
+            }
+        }
     }
 }
--- Models/Chamado.cs
+++ Models/Chamado.cs
@@ -2,12 +2,10 @@
 {
     public class Chamado
     {
-        public int NUM_CHAMADO { get; set; }
+        public int ID { get; set; }
         public string TITULO { get; set; }
-        public string DESCRICAO { get; set; }
-        public string SISTEMA { get; set; }
+        public string AREA { get; set; }
         public string SITUACAO { get; set; }
-        public DateTime DATA_SOLICITACAO { get; set; }
-        public DateTime DATA_SLA { get; set; }
+        public DateTime DATAABERTURA { get; set; }
     }
 }
